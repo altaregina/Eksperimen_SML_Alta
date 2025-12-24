8d72c520cc84b76982699f072bd145fa9ef8d839
Alta Regina <reginaalta078@gmail.com>
2025-12-24 21:55:16 +0700

    init Eksperimen SML

---
+++ Workflow-CI/modelling.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import joblib
+import os
+
+# Load dataset
+df = pd.read_csv("namadataset_preprocessing/heart_clean.csv")
+
+X = df.drop("target", axis=1)
+y = df["target"]
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+# Evaluate
+acc = accuracy_score(y_test, model.predict(X_test))
+print("Accuracy:", acc)
+
+# Save model
+os.makedirs("models", exist_ok=True)
+joblib.dump(model, "models/model.pkl")
